c74fc8a1d973b9e3335c000281260247a5dbd600
Austin <tashoecraft@gmail.com>
2015-08-19 17:20:46 -0400

    hey

--- StaticUI/app.js
+++ StaticUI/app.js
@@ -4,18 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
+var sass = require('node-sass-middleware');
+
+require('./models');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+
 
 var app = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +29,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use(
+  sass({
+    src: './public/stylesheets',
+    dest: './public/stylesheets',
+    debug: true
+  })
+);
+// api key AIzaSyCkOctgg_Jy1cZKlTokM6vhwgvY86mO5QU
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,12 +45,12 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  swig.setDefaults({ cache: false });
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- StaticUI/routes/index.js
+++ StaticUI/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Hotel = require('mongoose').model('Hotel');
+var Restaurant = require('mongoose').model('Restaurant');
+var Activity = require('mongoose').model('Activity');
+
+// function eHandler(err) {console.error(err)};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var temp = [  Hotel.find({}).exec(), Restaurant.find({}).exec(), Activity.find({}).exec()];
+    Promise.all(temp).then( function(elements){
+      res.render('index', {
+          all_hotels: elements[0],
+          all_restaurants: elements[1],
+          all_activities: elements[2]
+    });
+}).catch(null, console.log);
+
 });
+
+router.use('/bower_components', express.static('bower_components')
+);
 
 module.exports = router;
