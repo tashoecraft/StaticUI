db4af4bce4da9800af5dd534ecd66ffd53cd5e55
Austin <tashoecraft@gmail.com>
2015-08-19 12:49:37 -0400

    set up schema's

--- StaticUI/models/index.js
+++ StaticUI/models/index.js
@@ -4,11 +4,50 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
 var Place, Hotel, Activity, Restaurant, Day;
+var Schema = mongoose.Schema;
 
 var placeSchema = new Schema({
   address: { type: String, required: true },
   city: String,
   state:   String,
-  phone: { type: String, required: true },
-  location: { type: Date, default: Date.now }
+  phone: String,
+  location: [Number]
 });
+
+var hotelSchema = new Schema({
+  name: { type: String, required: true },
+  place: {mongoose.Schema.Types.ObjectId, ref: 'Place'},
+  num_stars:   {type: Number, min: 1, max:5},
+  amenities: [String]
+});
+
+var activitySchema = new Schema({
+  name: { type: String, required: true },
+  place: {mongoose.Schema.Types.ObjectId, ref: 'Place'},
+  age_range: String
+});
+
+var restaurantSchema = new Schema({
+  name: { type: String, required: true },
+  place: {mongoose.Schema.Types.ObjectId, ref: 'Place'},
+  cuisines:   String,
+  price: { type: String, required: true }
+});
+
+var daySchema = new Schema({
+
+});
+
+Place = mongoose.model('Place', placeSchema);
+Hotel = mongoose.model('Hotel', hotelSchema);
+Activity = mongoose.model('Activity', activitySchema);
+Restaurant = mongoose.model('Restaurant', restaurantSchema);
+Day = mongoose.model('Day', daySchema);
+
+module.exports = {
+  Place: Place,
+  Hotel: Hotel,
+  Activity: Activity,
+  Restaurant: Restaurant,
+  Day: Day
+}
